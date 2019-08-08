e53030bc6f062893721acb545f396cd3cea98b32
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-08 11:23:32 +0200

    rails new

---
+++ config/webpack/development.js
@@ -0,0 +1,18 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const environment = require('./environment')
+
+module.exports = environment.toWebpackConfig()
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+  )
+
+module.exports = environment
